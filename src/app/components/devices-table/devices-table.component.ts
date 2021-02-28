f459ca4f56137525b9aa31a4cd7b462faa57e1cd
May-line Jollet <mjollet@quantmetry.com>
2021-02-28 17:10:58 +0100

    Add pagination and filter

--- src/app/components/devices-table/devices-table.component.ts
+++ src/app/components/devices-table/devices-table.component.ts
@@ -1,16 +1,17 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {DeviceModel} from '../../services/device.model';
 import {MatTableDataSource} from '@angular/material/table';
 import {DevicesService} from '../../services/devices.service';
+import {MatPaginator} from '@angular/material/paginator';
 
 @Component({
   selector: 'app-devices-table',
   templateUrl: './devices-table.component.html',
   styleUrls: ['./devices-table.component.css']
 })
-export class DevicesTableComponent implements OnInit {
+export class DevicesTableComponent implements OnInit{
   rows: DeviceModel [];
-  data;
+  dataSource;
   selectedDevice?: DeviceModel;
   displayedColumns = [
     'serial_number',
@@ -29,14 +30,16 @@ export class DevicesTableComponent implements OnInit {
     'Sim id',
     'Voltage',
   ];
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
 
   constructor(private deviceService: DevicesService) {
   }
 
   ngOnInit(): void {
     this.deviceService.getDevices().subscribe(result => {
-        this.data = new MatTableDataSource(result);
-        console.log(this.data);
+        this.dataSource = new MatTableDataSource(result);
+        this.dataSource.paginator = this.paginator;
       }
     );
   }
@@ -44,6 +47,11 @@ export class DevicesTableComponent implements OnInit {
   onSelect(row: DeviceModel): void {
     this.selectedDevice = row;
     console.log(row);
+  }
+
+  applyFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
