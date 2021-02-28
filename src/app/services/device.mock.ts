c268a0fa282ae6d87561277add126643be9b7bb7
May-line Jollet <mjollet@quantmetry.com>
2021-02-28 21:19:32 +0100

    Unit test devices services

---
+++ src/app/services/device.mock.ts
@@ -0,0 +1,24 @@
+import {DeviceModel} from './device.model';
+
+export const mockDevices: DeviceModel [] = [
+  {
+    url: 'https://fake.url/46',
+    status: 'disconnected',
+    last_seen_at: '2021-02-28T20:04:48.391029',
+    connection_type: 'cellular',
+    mac_wifi: 'ad:f3:1b:4e:06:68',
+    sim_id: 'SomeFakeString',
+    voltage: 11.160086967849404,
+    serial_number: 'device_46'
+  },
+  {
+    url: 'https://fake.url/46',
+    status: 'disconnected',
+    last_seen_at: '2021-02-28T20:04:48.391029',
+    connection_type: 'cellular',
+    mac_wifi: 'ad:f3:1b:4e:06:68',
+    sim_id: 'SomeFakeString',
+    voltage: 11.160086967849404,
+    serial_number: 'device_46'
+  },
+];
--- src/app/services/devices.service.spec.ts
+++ src/app/services/devices.service.spec.ts
@@ -1,16 +1,31 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DevicesService } from './devices.service';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {mockDevices} from './device.mock';
 
 describe('DevicesService', () => {
   let service: DevicesService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(DevicesService);
+    httpMock = TestBed.inject(HttpTestingController);
+
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should request get devices endpoint', () => {
+    service.getDevices().subscribe(data => {
+      expect(data).toEqual(mockDevices);
+    });
+    const request = httpMock.expectOne('http://localhost:8010/devices?n=10');
+    request.flush(mockDevices);
   });
 });
