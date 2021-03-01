d7f1c672421a4390f05793ca0c7f149f0bc87348
May-line Jollet <mjollet@quantmetry.com>
2021-03-01 16:01:28 +0100

    Delete devices/details/:id

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {DashboardComponent} from './views/dashboard/dashboard.component';
 import {DevicesComponent} from './views/devices/devices.component';
-import {DeviceDetailComponent} from './components/device-detail/device-detail.component';
 
 
 const routes: Routes = [
   {path: 'dashboard', component: DashboardComponent},
   {path: 'devices', component: DevicesComponent},
-  {path: 'devices/:serial_number', component: DeviceDetailComponent},
   {path: '', redirectTo: 'devices', pathMatch: 'full'},
 ];
 
